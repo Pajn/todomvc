6b7df21acf8c7c9e1db403eb3ab3342f0b960921
Jesper Håkansson <jesper@jesperh.se>
2016-03-07 12:48:09 +0100

    refactor: Replace tabs with spaces

--- app/components/todo-list/controller.js
+++ app/components/todo-list/controller.js
@@ -17,65 +17,65 @@ angular.module('todomvc')
   });
 
 function TodoListCtrl($routeParams, $filter, $scope) {
-	'use strict';
+  'use strict';
 
   var ctrl = this;
 
-	var todos = ctrl.todos = ctrl.store.todos;
+  var todos = ctrl.todos = ctrl.store.todos;
 
-	$scope.$watch('ctrl.todos', function () {
-		ctrl.remainingCount = $filter('filter')(todos, { completed: false }).length;
-		ctrl.completedCount = todos.length - ctrl.remainingCount;
-		ctrl.allChecked = !ctrl.remainingCount;
-	}, true);
+  $scope.$watch('ctrl.todos', function () {
+    ctrl.remainingCount = $filter('filter')(todos, { completed: false }).length;
+    ctrl.completedCount = todos.length - ctrl.remainingCount;
+    ctrl.allChecked = !ctrl.remainingCount;
+  }, true);
 
-	// As we now have a parent controller in the router, we don't need to wait for it to be finished
-	// anymore
+  // As we now have a parent controller in the router, we don't need to wait for it to be finished
+  // anymore
   var status = ctrl.status = $routeParams.status || '';
   ctrl.statusFilter = (ctrl.status === 'active') ?
     { completed: false } : (ctrl.status === 'completed') ?
     { completed: true } : {};
 
-	ctrl.addTodo = function () {
-		var newTodo = {
-			title: ctrl.newTodo.trim(),
-			completed: false
-		};
+  ctrl.addTodo = function () {
+    var newTodo = {
+      title: ctrl.newTodo.trim(),
+      completed: false
+    };
 
-		if (!newTodo.title) {
-			return;
-		}
+    if (!newTodo.title) {
+      return;
+    }
 
-		ctrl.saving = true;
-		ctrl.store.insert(newTodo)
-			.then(function success() {
-				ctrl.newTodo = '';
-			})
-			.finally(function () {
-				ctrl.saving = false;
-			});
-	};
+    ctrl.saving = true;
+    ctrl.store.insert(newTodo)
+      .then(function success() {
+        ctrl.newTodo = '';
+      })
+      .finally(function () {
+        ctrl.saving = false;
+      });
+  };
 
-	ctrl.toggleCompleted = function (todo, completed) {
-		if (angular.isDefined(completed)) {
-			todo.completed = completed;
-		}
+  ctrl.toggleCompleted = function (todo, completed) {
+    if (angular.isDefined(completed)) {
+      todo.completed = completed;
+    }
 
-		ctrl.store.put(todo)
-			.then(function success() {}, function error() {
-				todo.completed = !todo.completed;
-			});
-	};
+    ctrl.store.put(todo)
+      .then(function success() {}, function error() {
+        todo.completed = !todo.completed;
+      });
+  };
 
-	ctrl.clearCompletedTodos = function () {
-		ctrl.store.clearCompleted();
-	};
+  ctrl.clearCompletedTodos = function () {
+    ctrl.store.clearCompleted();
+  };
 
-	ctrl.markAll = function (completed) {
-		todos.forEach(function (todo) {
-			if (todo.completed !== completed) {
-				ctrl.toggleCompleted(todo, completed);
-			}
-		});
-	};
+  ctrl.markAll = function (completed) {
+    todos.forEach(function (todo) {
+      if (todo.completed !== completed) {
+        ctrl.toggleCompleted(todo, completed);
+      }
+    });
+  };
 }
