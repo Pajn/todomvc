2dc4c122a11987de8303ef70ac82d6d57aabc998
Jesper Håkansson <jesper@jesperh.se>
2016-03-07 12:48:55 +0100

    refactor: Replace tabs with spaces

--- app/app.js
+++ app/app.js
@@ -6,29 +6,29 @@
  * @type {angular.Module}
  */
 angular.module('todomvc', ['ngRoute', 'ngResource'])
-	.config(function ($routeProvider) {
-		'use strict';
+  .config(function ($routeProvider) {
+    'use strict';
 
-		var routeConfig = {
-			template: '<todo-list store="store" />',
-			controller: function (store, $scope) {
-				$scope.store = store;
-			},
-			resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
-			}
-		};
+    var routeConfig = {
+      template: '<todo-list store="store" />',
+      controller: function (store, $scope) {
+        $scope.store = store;
+      },
+      resolve: {
+        store: function (todoStorage) {
+          // Get the correct module (API or localStorage).
+          return todoStorage.then(function (module) {
+            module.get(); // Fetch the todo records in the background.
+            return module;
+          });
+        }
+      }
+    };
 
-		$routeProvider
-			.when('/', routeConfig)
-			.when('/:status', routeConfig)
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+    $routeProvider
+      .when('/', routeConfig)
+      .when('/:status', routeConfig)
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- app/components/todo-item/controller.js
+++ app/components/todo-item/controller.js
@@ -23,72 +23,72 @@ angular.module('todomvc')
   });
 
 function TodoCtrl() {
-	'use strict';
+  'use strict';
 
   var ctrl = this;
 
-	ctrl.newTodo = '';
-	ctrl.editedTodo = null;
+  ctrl.newTodo = '';
+  ctrl.editedTodo = null;
 
-	ctrl.editTodo = function () {
-		ctrl.editedTodo = ctrl.todo;
-		// Clone the original todo to restore it on demand.
-		ctrl.originalTodo = angular.extend({}, ctrl.todo);
-	};
+  ctrl.editTodo = function () {
+    ctrl.editedTodo = ctrl.todo;
+    // Clone the original todo to restore it on demand.
+    ctrl.originalTodo = angular.extend({}, ctrl.todo);
+  };
 
-	ctrl.saveEdits = function (event) {
+  ctrl.saveEdits = function (event) {
     var todo = ctrl.todo;
 
-		// Blur events are automatically triggered after the form submit event.
-		// This does some unfortunate logic handling to prevent saving twice.
-		if (event === 'blur' && ctrl.saveEvent === 'submit') {
-			ctrl.saveEvent = null;
-			return;
-		}
+    // Blur events are automatically triggered after the form submit event.
+    // This does some unfortunate logic handling to prevent saving twice.
+    if (event === 'blur' && ctrl.saveEvent === 'submit') {
+      ctrl.saveEvent = null;
+      return;
+    }
 
-		ctrl.saveEvent = event;
+    ctrl.saveEvent = event;
 
-		if (ctrl.reverted) {
-			// Todo edits were reverted-- don't save.
-			ctrl.reverted = null;
-			return;
-		}
+    if (ctrl.reverted) {
+      // Todo edits were reverted-- don't save.
+      ctrl.reverted = null;
+      return;
+    }
 
-		todo.title = todo.title.trim();
+    todo.title = todo.title.trim();
 
-		if (todo.title === ctrl.originalTodo.title) {
-			ctrl.editedTodo = null;
-			return;
-		}
+    if (todo.title === ctrl.originalTodo.title) {
+      ctrl.editedTodo = null;
+      return;
+    }
 
-		ctrl.store[todo.title ? 'put' : 'delete'](todo)
-			.then(function success() {}, function error() {
-				todo.title = ctrl.originalTodo.title;
-			})
-			.finally(function () {
-				ctrl.editedTodo = null;
-			});
-	};
+    ctrl.store[todo.title ? 'put' : 'delete'](todo)
+      .then(function success() {}, function error() {
+        todo.title = ctrl.originalTodo.title;
+      })
+      .finally(function () {
+        ctrl.editedTodo = null;
+      });
+  };
 
-	ctrl.revertEdits = function () {
-		todos[todos.indexOf(todo)] = ctrl.originalTodo;
-		ctrl.editedTodo = null;
-		ctrl.originalTodo = null;
-		ctrl.reverted = true;
-	};
+  ctrl.revertEdits = function () {
+    todos[todos.indexOf(todo)] = ctrl.originalTodo;
+    ctrl.editedTodo = null;
+    ctrl.originalTodo = null;
+    ctrl.reverted = true;
+  };
 
-	ctrl.removeTodo = function () {
-		ctrl.store.delete(ctrl.todo);
-	};
+  ctrl.removeTodo = function () {
+    ctrl.store.delete(ctrl.todo);
+  };
 
-	ctrl.saveTodo = function () {
-		ctrl.store.put(ctrl.todo);
-	};
+  ctrl.saveTodo = function () {
+    ctrl.store.put(ctrl.todo);
+  };
 
-	ctrl.toggleCompleted = function (completed) {
-		ctrl.store.put(ctrl.todo)
-			.then(function success() {}, function error() {
-				ctrl.todo.completed = !ctrl.todo.completed;
-			});
-	};
+  ctrl.toggleCompleted = function (completed) {
+    ctrl.store.put(ctrl.todo)
+      .then(function success() {}, function error() {
+        ctrl.todo.completed = !ctrl.todo.completed;
+      });
+  };
 }
