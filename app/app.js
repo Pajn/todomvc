e73033ca45ebe6c4696db86adefa05fd4360238b
Jesper Håkansson <jesper@jesperh.se>
2016-03-07 12:50:08 +0100

    refactor: Replace tabs with spaces

--- app/app.js
+++ app/app.js
@@ -6,27 +6,27 @@
  * @type {angular.Module}
  */
 angular.module('todomvc', ['ngRoute', 'ngResource'])
-	.config(function ($routeProvider) {
-		'use strict';
+  .config(function ($routeProvider) {
+    'use strict';
 
-		var routeConfig = {
-			controller: 'TodoListCtrl',
-			templateUrl: '/app/components/todo-list/view.html',
-			resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
-			}
-		};
+    var routeConfig = {
+      controller: 'TodoListCtrl',
+      templateUrl: '/app/components/todo-list/view.html',
+      resolve: {
+        store: function (todoStorage) {
+          // Get the correct module (API or localStorage).
+          return todoStorage.then(function (module) {
+            module.get(); // Fetch the todo records in the background.
+            return module;
+          });
+        }
+      }
+    };
 
-		$routeProvider
-			.when('/', routeConfig)
-			.when('/:status', routeConfig)
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+    $routeProvider
+      .when('/', routeConfig)
+      .when('/:status', routeConfig)
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- app/components/todo-list/controller.js
+++ app/components/todo-list/controller.js
@@ -6,66 +6,66 @@
  * - exposes the model to the template and provides event handlers
  */
 angular.module('todomvc')
-	.controller('TodoListCtrl', function TodoListCtrl($scope, $routeParams, $filter,store) {
-		'use strict';
+  .controller('TodoListCtrl', function TodoListCtrl($scope, $routeParams, $filter,store) {
+    'use strict';
 
-		var todos = $scope.todos = store.todos;
+    var todos = $scope.todos = store.todos;
 
     $scope.store = store;
 
-		$scope.$watch('todos', function () {
-			$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
-			$scope.completedCount = todos.length - $scope.remainingCount;
-			$scope.allChecked = !$scope.remainingCount;
-		}, true);
+    $scope.$watch('todos', function () {
+      $scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+      $scope.completedCount = todos.length - $scope.remainingCount;
+      $scope.allChecked = !$scope.remainingCount;
+    }, true);
 
-		// Monitor the current route for changes and adjust the filter accordingly.
-		$scope.$on('$routeChangeSuccess', function () {
-			var status = $scope.status = $routeParams.status || '';
-			$scope.statusFilter = (status === 'active') ?
-				{ completed: false } : (status === 'completed') ?
-				{ completed: true } : {};
-		});
+    // Monitor the current route for changes and adjust the filter accordingly.
+    $scope.$on('$routeChangeSuccess', function () {
+      var status = $scope.status = $routeParams.status || '';
+      $scope.statusFilter = (status === 'active') ?
+        { completed: false } : (status === 'completed') ?
+        { completed: true } : {};
+    });
 
-		$scope.addTodo = function () {
-			var newTodo = {
-				title: $scope.newTodo.trim(),
-				completed: false
-			};
+    $scope.addTodo = function () {
+      var newTodo = {
+        title: $scope.newTodo.trim(),
+        completed: false
+      };
 
-			if (!newTodo.title) {
-				return;
-			}
+      if (!newTodo.title) {
+        return;
+      }
 
-			$scope.saving = true;
-			store.insert(newTodo)
-				.then(function success() {
-					$scope.newTodo = '';
-				})
-				.finally(function () {
-					$scope.saving = false;
-				});
-		};
+      $scope.saving = true;
+      store.insert(newTodo)
+        .then(function success() {
+          $scope.newTodo = '';
+        })
+        .finally(function () {
+          $scope.saving = false;
+        });
+    };
 
-		$scope.toggleCompleted = function (todo, completed) {
-			if (angular.isDefined(completed)) {
-				todo.completed = completed;
-			}
-			store.put($scope.todo, todos.indexOf($scope.todo))
-				.then(function success() {}, function error() {
-					$scope.todo.completed = !$scope.todo.completed;
-				});
-		};
+    $scope.toggleCompleted = function (todo, completed) {
+      if (angular.isDefined(completed)) {
+        todo.completed = completed;
+      }
+      store.put($scope.todo, todos.indexOf($scope.todo))
+        .then(function success() {}, function error() {
+          $scope.todo.completed = !$scope.todo.completed;
+        });
+    };
 
-		$scope.clearCompletedTodos = function () {
-			store.clearCompleted();
-		};
+    $scope.clearCompletedTodos = function () {
+      store.clearCompleted();
+    };
 
-		$scope.markAll = function (completed) {
-			todos.forEach(function (todo) {
-				if (todo.completed !== completed) {
-					$scope.toggleCompleted(todo, completed);
-				}
-			});
-		};
-	});
+    $scope.markAll = function (completed) {
+      todos.forEach(function (todo) {
+        if (todo.completed !== completed) {
+          $scope.toggleCompleted(todo, completed);
+        }
+      });
+    };
+  });
